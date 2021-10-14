06fdeb84c2d452eecbfb7e246cc82118eb280a82
Saikiran-Alagatham <saikiran.ask007@gmail.com>
2021-10-14 05:29:25 +0000

    chnages to ec2

--- deploy/ec2.tf
+++ deploy/ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "instances" {
+resource "aws_instance" "cheap_worker" {
   count                     = local.LENGTH
   ami                       = "ami-074df373d6bafa625"
   instance_type             = "t3.micro"
@@ -8,19 +8,37 @@ resource "aws_instance" "instances" {
   }
 }
 
+
 resource "aws_route53_record" "records" {
   count                     = local.LENGTH
   name                      = element(var.COMPONENTS, count.index)
   type                      = "A"
-  zone_id                   = "Z04058843B1ZOXKPWHN2J"
+  zone_id                   = "Z00620242Y7LFBGQOS2W8"
   ttl                       = 300
-  records                   = [element(aws_instance.instances.*.private_ip, count.index)]
+  records                   = [element(aws_instance.cheap_worker.*.private_ip, count.index)]
 }
 
 
 
-locals {
-  LENGTH    = length(var.COMPONENTS)
+resource "null_resource" "run-shell-scripting" {
+  depends_on                = [aws_route53_record.records]
+  count                     = local.LENGTH
+  provisioner "remote-exec" {
+    connection {
+      host                  = element(aws_instance.cheap_worker.*.public_ip, count.index)
+      user                  = "centos"
+      password              = "DevOps321"
+    }
+
+    inline = [
+    "cd /home/centos",
+    "rm -rf *",
+    "git clone https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps57/_git/shell-scripting",
+    "cd shell-scripting/roboshop",
+    "git pull",
+    "sudo make ${element(var.COMPONENTS, count.index)}"
+    ]
+  }
 }
 
 
