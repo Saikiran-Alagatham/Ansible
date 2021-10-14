828effbaf9ced21c50641d019236a31cdb337a5e
Saikiran-Alagatham <saikiran.ask007@gmail.com>
2021-10-14 05:41:06 +0000

    ec2 changes

--- deploy/ec2.tf
+++ deploy/ec2.tf
@@ -20,27 +20,6 @@ resource "aws_route53_record" "records" {
 
 
 
-resource "null_resource" "run-shell-scripting" {
-  depends_on                = [aws_route53_record.records]
-  count                     = local.LENGTH
-  provisioner "remote-exec" {
-    connection {
-      host                  = element(aws_instance.instances.*.public_ip, count.index)
-      user                  = "centos"
-      password              = "DevOps321"
-    }
-
-    inline = [
-    "cd /home/centos",
-    "rm -rf *",
-    "git clone https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps57/_git/shell-scripting",
-    "cd shell-scripting/roboshop",
-    "git pull",
-    "sudo make ${element(var.COMPONENTS, count.index)}"
-    ]
-  }
-}
-
 
 resource "local_file" "inventory-file" {
   content     = "[FRONTEND]\n${aws_instance.instances.*.private_ip[9]}\n[PAYMENT]\n${aws_instance.instances.*.private_ip[8]}\n[SHIPPING]\n${aws_instance.instances.*.private_ip[7]}\n[USER]\n${aws_instance.instances.*.private_ip[6]}\n[CATALOGUE]\n${aws_instance.instances.*.private_ip[5]}\n[CART]\n${aws_instance.instances.*.private_ip[4]}\n[REDIS]\n${aws_instance.instances.*.private_ip[3]}\n[RABBITMQ]\n${aws_instance.instances.*.private_ip[2]}\n[MONGODB]\n${aws_instance.instances.*.private_ip[1]}\n[MYSQL]\n${aws_instance.instances.*.private_ip[0]}\n"
