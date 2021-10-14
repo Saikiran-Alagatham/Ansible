a7ee2de3ad67d6c74c4ac34f1719258aceb01959
Saikiran-Alagatham <saikiran.ask007@gmail.com>
2021-10-14 00:33:16 +0000

    changing sg

--- deploy/ec2.tf
+++ deploy/ec2.tf
@@ -2,41 +2,11 @@ resource "aws_instance" "instances" {
   count                     = local.LENGTH
   ami                       = "ami-074df373d6bafa625"
   instance_type             = "t3.micro"
-  vpc_security_group_ids    =  [aws_security_group.allow_ssh_single_server.id]
+  vpc_security_group_ids    =  ["sg-04371d9790f1294b1"]
   tags                      = {
     Name                    = element(var.COMPONENTS, count.index)
   }
 }
-
-# resource "aws_ec2_tag" "name-tag" {
-#    count                     = local.LENGTH
-#    resource_id               = element(aws_instance.instances.*.instance_id, count.index)
-#    key                       = "Name"
-#    value                     = element(var.COMPONENTS, count.index)
-# }
-
-resource "aws_security_group" "allow_ssh_single_server" {
-    name            = "allow_ssh_single_server"
-    description     = "allow_ssh_single_server"
-    
-    ingress {
-        from_port       = 22
-        to_port         = 22
-        protocol        = "tcp"
-        cidr_blocks     = ["0.0.0.0/0"]
-    }
-
-
-    egress {
-        from_port       = 0
-        to_port         = 0
-        protocol        = "-1"
-        cidr_blocks     = ["0.0.0.0/0"]
-    }
-}
-
-
-
 
 resource "aws_route53_record" "records" {
   count                     = local.LENGTH
